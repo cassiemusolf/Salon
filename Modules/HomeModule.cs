3e7cf333b7bf0006ed689f00ac490e97c510a454
Cassie Musolf <cassiemusolf@gmail.com>
2017-02-24 14:20:41 -0800

    add form pages for both the client and stylist, route them to other pages.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -4,22 +4,40 @@ using Nancy.ViewEngines.Razor;
 
 namespace SalonApp
 {
-  public class HomeModule : NancyModule
-  {
-    public HomeModule()
+    public class HomeModule : NancyModule
     {
-      Get["/"] = _ => {
-        List<Stylist> AllStylists = Stylist.GetAll();
-        return View["index.cshtml", AllStylists];
-      };
-      Get["/clients"] = _ => {
-          List<Client> AllClients = Client.GetAll();
-          return View["clients.csthml, AllClients"];
-      };
-      Get["/stylists"] = _ => {
-           List<Stylist> AllStylists = Stylist.GetAll();
-           return View["stylists.cshtml", AllStylists];
-       };
-  }
-}
+        public HomeModule()
+        {
+            Get["/"] = _ => {
+                List<Stylist> AllStylists = Stylist.GetAll();
+                return View["index.cshtml", AllStylists];
+            };
+            Get["/clients"] = _ => {
+                List<Client> AllClients = Client.GetAll();
+                return View["clients.csthml, AllClients"];
+            };
+            Get["/stylists"] = _ => {
+                List<Stylist> AllStylists = Stylist.GetAll();
+                return View["stylists.cshtml", AllStylists];
+            };
+            Get["/stylists/new"] = _ => {
+                return View["stylist-form.cshtml"];
+            };
+            Post["/stylists/new"] = _ => {
+                Stylist newStylist = new Stylist(Request.Form["stylist-name"], Request.Form["stylist-phone"]);
+                newStylist.Save();
+                List<Stylist> AllStylists = Stylist.GetAll();
+                return View["stylists.cshtml", AllStylists];
+            };
+            Get["/clients/new"] = _ => {
+                List<Client> AllClients = Client.GetAll();
+                return View["clients_form.cshtml", AllClients];
+            };
+            Post["/clients/new"] = _ => {
+                Client newClient = new Client(Request.Form["client-name"], Request.Form["client-phone"], Request.Form["stylist-name"]);
+                newClient.Save();
+                return View["clients.cshtml"];
+            };
+        }
+    }
 }
